0c734e9596ae602ab07ec0a6b012f629cf399907
frectonz <fraol0912@gmail.com>
2023-03-26 16:37:10 +0300

    feat: :sparkles: add `programs` section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head";
 import { FC } from "react";
+import Head from "next/head";
 
 import Logo from "@/assets/Logo";
 import Nav from "@/components/nav/Nav";
@@ -83,62 +83,107 @@ const HeroSection = () => (
 				offer you the opportunity to display your work publicly and reach people
 				who are interested in developing their skills.
 			</p>
-			<form className="flex justify-center group w-full ">
-				<input
-					type="email"
-					placeholder="Enter your email"
-					className="py-2 px-4 w-80 rounded-l border border-gray-300 focus:border-brand-blue focus:outline-none focus:ring-1 focus:ring-brand-blue  text-gray-500"
-				/>
-				<button className="text-white py-2 px-4 bg-brand-black group-hover:bg-brand-blue group-focus:border-brand-blue rounded-r">
-					Become a Member
-				</button>
-			</form>
 		</div>
 	</section>
 );
 
-const Projects = () => (
-	<div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-		<div className="absolute inset-0">
-			<div className="bg-white h-1/3 sm:h-2/3" />
-		</div>
-		<div className="relative max-w-7xl mx-auto">
-			<div className="text-center">
-				<h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-					Projects
-				</h2>
-				<p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero
-					labore natus atque, ducimus sed.
-				</p>
-			</div>
-			<div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-				{posts.map((post, i) => (
-					<Project key={`project-${i}`} post={post} />
-				))}
-			</div>
-		</div>
-	</div>
-);
+const programs = [
+	{
+		name: "Debugging Session - Wednesday",
+		description:
+			"Ask your questions about programming and get answers from experienced developers.",
+		icon: () => (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className="w-6 h-6"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0112 12.75zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 01-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 002.248-2.354M12 12.75a2.25 2.25 0 01-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 00-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 01.4-2.253M12 8.25a2.25 2.25 0 00-2.248 2.146M12 8.25a2.25 2.25 0 012.248 2.146M8.683 5a6.032 6.032 0 01-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0115.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 00-.575-1.752M4.921 6a24.048 24.048 0 00-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 01-5.223 1.082"
+				/>
+			</svg>
+		),
+	},
+	{
+		name: "Open Mic Discussion - Sunday",
+		description:
+			"Join our weekly discussion about the latest technologies and trends.",
+		icon: () => (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className="w-6 h-6"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
+				/>
+			</svg>
+		),
+	},
+	{
+		name: "Chat with other developers - 24/7",
+		description: "Chat with other developers and share your knowledge.",
+		icon: () => (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className="w-6 h-6"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+				/>
+			</svg>
+		),
+	},
+];
 
-const Project: FC<{
-	post: { title: string; href: string; description: string; imageUrl: string };
-}> = ({ post }) => (
-	<div
-		key={post.title}
-		className="flex flex-col rounded-lg shadow-lg overflow-hidden"
-	>
-		<div className="flex-shrink-0">
-			<picture>
-				<img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
-			</picture>
-		</div>
-		<div className="flex-1 bg-white p-6 flex flex-col justify-between">
-			<div className="flex-1">
-				<a href={post.href} className="block mt-2">
-					<p className="text-xl font-semibold text-gray-900">{post.title}</p>
-					<p className="mt-3 text-base text-gray-500">{post.description}</p>
-				</a>
+const Programs = () => (
+	<div className="relative bg-white py-16 sm:py-24 lg:py-32">
+		<div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+			<h2 className="text-base font-semibold uppercase tracking-wider text-indigo-600">
+				Our Programs
+			</h2>
+			<p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+				Meet our Community Members
+			</p>
+
+			<div className="mt-12">
+				<div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+					{programs.map((feature) => (
+						<div key={feature.name} className="pt-6 h-full">
+							<div className="flow-root rounded-lg bg-gray-50 px-6 pb-8 h-full">
+								<div className="-mt-6">
+									<div>
+										<span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg text-white">
+											<feature.icon />
+										</span>
+									</div>
+									<h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">
+										{feature.name}
+									</h3>
+									<p className="mt-5 text-base text-gray-500">
+										{feature.description}
+									</p>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	</div>
@@ -266,7 +311,7 @@ export default function Home() {
 			<main>
 				<Nav />
 				<HeroSection />
-				<Projects />
+				<Programs />
 				<AboutUs />
 				<Partners />
 			</main>

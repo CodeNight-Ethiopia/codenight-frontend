f49b723eded406962c34f6af40da5bdbc9b4a4ce
frectonz <fraol0912@gmail.com>
2023-03-26 15:43:52 +0300

    feat: :sparkles: a "special thanks" section

--- pages/index.tsx
+++ pages/index.tsx
@@ -196,41 +196,22 @@ const AboutUs = () => (
 );
 
 const Partners = () => (
-	<div className="bg-gray-50">
-		<div className="max-w-2xl mx-auto py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8">
-			<h2 className="text-3xl text-center tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-				OUR PARTNERS
-			</h2>
-
-			<div className="grid grid-cols-1 my-10 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8">
-				<picture>
-					<img
-						src={"https://www.hahucloud.com/images/hahucloud-logo.svg"}
-						alt=""
-						className="h-24 w-auto"
-					/>
-				</picture>
-				<picture>
-					<img
-						src={"https://www.hahucloud.com/images/hahucloud-logo.svg"}
-						alt=""
-						className="h-24 w-auto"
-					/>
-				</picture>
-				<picture>
-					<img
-						src={"https://www.hahucloud.com/images/hahucloud-logo.svg"}
-						alt=""
-						className="h-24 w-auto"
-					/>
-				</picture>
-				<picture>
-					<img
-						src={"https://www.hahucloud.com/images/hahucloud-logo.svg"}
-						alt=""
-						className="h-24 w-auto"
-					/>
-				</picture>
+	<div className="mb-24">
+		<div className="max-w-7xl mx-auto py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+			<h2 className="text-3xl font-extrabold text-center">Special Thanks To</h2>
+			<div className="flow-root mt-8 lg:mt-10">
+				<div className="-mt-4 -ml-8 flex flex-wrap justify-center lg:-ml-4">
+					<div className="mt-4 ml-8 flex flex-grow flex-shrink-0 lg:flex-grow-0 lg:ml-4">
+						<picture>
+							<img className="h-12" src="/hahucloud-logo.png" alt="HahuCloud" />
+						</picture>
+					</div>
+					<div className="mt-4 ml-8 flex flex-grow flex-shrink-0 lg:flex-grow-0 lg:ml-4">
+						<picture>
+							<img className="h-12" src="/chapa-logo.png" alt="Chapa" />
+						</picture>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>

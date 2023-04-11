bde3ea6277f5b8d3209282024ae8852fcca37e1c
k4l3b4 <kaleba647@gmail.com>
2023-04-11 11:29:56 +0300

    no update

--- data/index.ts
+++ data/index.ts
@@ -1,46 +1,46 @@
 export const NavigationMenuData = [
-  {
-    name: "Home",
-    link: "/",
-  },
-  {
-    name: "About Us",
-    link: "/about-us",
-  },
-  {
-    name: "Read",
-    link: "/read",
-  },
-  {
-    name: "FAQ's",
-    link: "/faq",
-  },
-  {
-    name: "Contact Us",
-    link: "/contact-us",
-  },
+	{
+		name: "Home",
+		link: "/",
+	},
+	{
+		name: "About Us",
+		link: "/about-us",
+	},
+	{
+		name: "Read",
+		link: "/read",
+	},
+	{
+		name: "FAQ's",
+		link: "/faq",
+	},
+	{
+		name: "Contact Us",
+		link: "/contact-us",
+	},
 ];
 
 export const FAQData = [
-  {
-    title: "What is codenight?",
-    desc: "Code-night is a developer community based in Ethiopia that aims to connect, support, and empower developers to build innovative solutions and grow their skills.",
-  },
-  {
-    title: "Who can join Codenight?",
-    desc: "Anyone who is interested in programming and software development can join Codenight. The community is open to developers of all levels, from beginners to experienced professionals.",
-  },
-  {
-    title:
-      "What programming languages and technologies does Codenight focus on",
-    desc: "Codenight is an inclusive community that welcomes developers of all backgrounds and skill levels. While the community doesn't focus on any particular language or technology, members are encouraged to share their knowledge and expertise with others.",
-  },
-  {
-    title: "How can I stay up-to-date with Codenight activities",
-    desc: "To stay up-to-date with Codenight activities, you can follow the community's social media accounts, or check our website for the latest updates. You can also engage with other members of the community to stay informed about upcoming events and opportunities.",
-  },
-  {
-    title: "How can I suggest ideas for Codenight activities or events?",
-    desc: "If you have ideas for Codenight activities or events, you can share them with the community through their social media groups or by reaching out to the organizers directly. The community is always open to new ideas and welcomes input from its members.",
-  },
+	{
+		title: "What is codenight?",
+		desc: "Code-night is a developer community based in Ethiopia that aims to connect, support, and empower developers to build innovative solutions and grow their skills.",
+	},
+	{
+		title: "Who can join Codenight?",
+		desc: "Anyone who is interested in programming and software development can join Codenight. The community is open to developers of all levels, from beginners to experienced professionals.",
+	},
+	{
+		title:
+			"What programming languages and technologies does Codenight focus on",
+		desc: "Codenight is an inclusive community that welcomes developers of all backgrounds and skill levels. While the community doesn't focus on any particular language or technology, members are encouraged to share their knowledge and expertise with others.",
+	},
+	{
+		title: "How can I stay up-to-date with Codenight activities",
+		desc: "To stay up-to-date with Codenight activities, you can follow the community's social media accounts, or check our website for the latest updates. You can also engage with other members of the community to stay informed about upcoming events and opportunities.",
+	},
+	{
+		title: "How can I suggest ideas for Codenight activities or events?",
+		desc: "If you have ideas for Codenight activities or events, you can share them with the community through their social media groups or by reaching out to the organizers directly. The community is always open to new ideas and welcomes input from its members.",
+	},
 ];

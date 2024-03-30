981f3305dac8d7e12b15a21321c3512310e151f5
Yeabsira Mekuria <yabumek46@gmail.com>
2024-03-30 13:02:07 +0300

    add the nav and stickybottombanner to layout

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import StickyBottomBanner from "@/components/StickyBottomBanner";
+import Nav from "@/components/nav/Nav";
+import "@/styles/globals.css";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <main>
+          <Nav />
+          {children}
+          <StickyBottomBanner />
+        </main>
+      </body>
+    </html>
+  );
+}

b47154e54b4635aa602f1d28503659d8b240852f
Kinfe123 <kinfishtech@gmail.com>
2024-04-02 00:27:07 +0300

    fix: footer fix

--- components/events/get-started.tsx
+++ components/events/get-started.tsx
@@ -1,55 +1,75 @@
+import Image from 'next/image'
+
 import { Button } from '@/components/ui/button'
-import { Wrapper } from "./wrapper"
-import { GridishPattern } from './gridish-pattern'
+import { Wrapper } from './wrapper'
+import backgroundImage from '../../public/background-2.jpg'
+import { SectionHeading } from './section-heading'
+
+function ArrowRightIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg aria-hidden="true" viewBox="0 0 24 24" {...props}>
+      <path
+        d="m14 7 5 5-5 5M19 12H5"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
 
 export function GetStarted() {
   return (
-    <section
-      id="get-started"
-      aria-label="Get Started"
-      className="scroll-mt-14 bg-blue-600 sm:scroll-mt-32"
-    >
-      <div className="overflow-hidden lg:relative">
-        <Wrapper
-          size="md"
-          className="relative grid  font-display grid-cols-1 items-end gap-y-12 py-20 lg:static lg:grid-cols-2 lg:py-28 xl:py-32"
-        >
-          <GridishPattern className="absolute -top-32 left-0 w-full sm:-top-5 sm:left-3/4 sm:ml-8 sm:w-auto md:left-2/3 lg:left-auto lg:right-2 lg:ml-0 xl:left-2/3 xl:right-auto" />
-          <div>
-            <h2 className="font-display text-5xl font-extrabold tracking-tight text-white sm:w-3/4 sm:text-6xl md:w-2/3 lg:w-auto">
-              Get your self confirmed.
-            </h2>
-            <p className="mt-4 text-lg tracking-tight text-blue-200">
-              Enter your email address and I’ll send you a notice when events happens
-            </p>
-          </div>
-          <form className="lg:pl-16">
-            <h3 className="text-base font-medium tracking-tight text-white">
-              You will be notified about everything in CodeNight{' '}
-              <span aria-hidden="true">&rarr;</span>
-            </h3>
-            <div className="mt-4 sm:relative sm:flex sm:items-center sm:py-0.5 sm:pr-2.5">
-              <div className="relative sm:static sm:flex-auto">
+    <section id="newsletter" aria-label="Newsletter" className='font-display'>
+      <Wrapper size='lg' className='rounded-3xl'>
+      <SectionHeading number='07' className='mb-5'>
+        Subscribe 
+
+      </SectionHeading>
+        <div className="relative font-display -mx-4 overflow-hidden bg-indigo-50 px-4 py-20 sm:-mx-6 sm:px-6 md:mx-0 md:rounded-[2.0rem] md:px-16 xl:px-24 xl:py-36">
+          <Image
+            className="absolute left-1/2 top-0 translate-x-[-10%] translate-y-[-45%] lg:translate-x-[-32%]"
+            src={backgroundImage}
+            alt=""
+            width={919}
+            height={1351}
+            unoptimized
+          />
+          <div className="relative mx-auto grid max-w-2xl grid-cols-1 gap-x-32 gap-y-14 xl:max-w-none xl:grid-cols-2">
+            <div>
+              <p className="font-display text-4xl font-medium tracking-tighter text-blue-900 sm:text-5xl">
+                Stay up to date
+              </p>
+              <p className="mt-4 text-lg tracking-tight text-blue-900">
+                Get updates on all of our events and be the first to get
+                notified when tickets go on sale.
+              </p>
+            </div>
+            <form>
+              <h3 className="text-lg font-semibold tracking-tight text-blue-900">
+                Sign up to our newsletter <span aria-hidden="true">&darr;</span>
+              </h3>
+              <div className="mt-5 flex rounded-3xl bg-white py-2.5 pr-2.5 shadow-xl shadow-blue-900/5 focus-within:ring-2 focus-within:ring-blue-900">
                 <input
                   type="email"
-                  id="email-address"
                   required
-                  aria-label="Email address"
                   placeholder="Email address"
-                  className="peer relative z-10 w-full appearance-none bg-transparent px-4 py-2 text-base text-white placeholder:text-white/70 focus:outline-none sm:py-3"
+                  aria-label="Email address"
+                  className="-my-2.5 flex-auto bg-transparent pl-6 pr-2.5 text-base text-slate-900 placeholder:text-slate-400 focus:outline-none"
                 />
-                <div className="absolute inset-0 rounded-md border border-white/20 peer-focus:border-blue-300 peer-focus:bg-blue-500 peer-focus:ring-1 peer-focus:ring-blue-300 sm:rounded-xl" />
+                <Button type="submit" className='rounded-3xl' variant={'blue'}>
+                  <span className="sr-only sm:not-sr-only">Sign up today</span>
+                  <span className="sm:hidden">
+                    <ArrowRightIcon className="h-6 w-6" />
+                  </span>
+                </Button>
               </div>
-              <Button
-                type="submit"
-                className="mt-4 text-black bg-white  w-full sm:relative sm:z-10 sm:mt-0 sm:w-auto sm:flex-none hover:bg-blue-600 hover:text-white"
-              >
-                Meet the devs
-              </Button>
-            </div>
-          </form>
-        </Wrapper>
-      </div>
+            </form>
+          </div>
+        </div>
+      </Wrapper>
     </section>
   )
 }
